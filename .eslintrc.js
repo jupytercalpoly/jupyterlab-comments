1ba7be0bb7f9e5a9cf01f0271a6dcabc84d228bf
Srirag Vuppala <59373265+srirag-vuppala@users.noreply.github.com>
2021-06-16 15:09:56 -0700

    Refactored eslintrc to enforce consistent code style

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,81 @@
 module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    commonjs: true,
+    node: true,
+    'jest/globals': true
+  },
+  globals: {
+    JSX: 'readonly'
+  },
+  root: true,
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended'
+    'prettier/@typescript-eslint',
+    'plugin:react/recommended',
+    'plugin:jest/recommended'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module'
+    project: 'tsconfig.eslint.json'
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'jest'],
   rules: {
+    '@typescript-eslint/no-floating-promises': ['error', { ignoreVoid: true }],
     '@typescript-eslint/naming-convention': [
       'error',
       {
-        'selector': 'interface',
-        'format': ['PascalCase'],
-        'custom': {
-          'regex': '^I[A-Z]',
-          'match': true
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^I[A-Z]',
+          match: true
         }
       }
     ],
     '@typescript-eslint/no-unused-vars': ['warn', { args: 'none' }],
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-namespace': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
-    '@typescript-eslint/quotes': [
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/ban-ts-comment': ['warn', { 'ts-ignore': true }],
+    '@typescript-eslint/ban-types': 'warn',
+    '@typescript-eslint/no-non-null-asserted-optional-chain': 'warn',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/triple-slash-reference': 'warn',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    'jest/no-conditional-expect': 'warn',
+    'jest/valid-title': 'warn',
+    'no-inner-declarations': 'off',
+    'no-prototype-builtins': 'off',
+    'no-control-regex': 'warn',
+    'no-undef': 'warn',
+    'no-case-declarations': 'warn',
+    'no-useless-escape': 'off',
+    'prefer-const': 'off',
+    'react/prop-types': 'warn',
+    'sort-imports': [
       'error',
-      'single',
-      { avoidEscape: true, allowTemplateLiterals: false }
-    ],
-    curly: ['error', 'all'],
-    eqeqeq: 'error',
-    'prefer-arrow-callback': 'error'
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+        allowSeparatedGroups: false
+      }
+    ]
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
   }
 };
+
