effd86b7e2f9ddb1b66a13ad10d70e6c7a30f76e
Srirag Vuppala <59373265+srirag-vuppala@users.noreply.github.com>
2021-06-17 10:17:38 -0700

    rename tsconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.eslint.json'
+    project: 'tsconfig.json'
   },
   plugins: ['@typescript-eslint', 'jest'],
   rules: {
