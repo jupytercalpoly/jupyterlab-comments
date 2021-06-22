e3969ed3ae602a5e33098455cdce1eb1d42ac9b0
rrn9902 <nair.rahul992@gmail.com>
2021-06-21 17:17:00 -0700

    Commenting Reply Proof of Concept

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,12 @@ import {
 } from '@jupyterlab/application';
 
 import { InputDialog } from '@jupyterlab/apputils';
-
+import * as Widgets from '@lumino/widgets';
+import * as Icons from '@jupyterlab/ui-components';
 import { INotebookTracker } from '@jupyterlab/notebook';
 
 namespace CommandIDs {
   export const addComment = 'jl-chat:add-comment';
-
   export const renderComment = 'jl-chat:render-comment';
 }
 
@@ -21,6 +21,22 @@ const plugin: JupyterFrontEndPlugin<void> = {
   autoStart: true,
   requires: [INotebookTracker],
   activate: (app: JupyterFrontEnd, nbTracker: INotebookTracker) => {
+
+    let panel = new Widgets.Panel();
+    panel.id = "Comments";
+    panel.title.icon = Icons.listIcon;
+    app.shell.add(panel, 'right', {rank:500});
+    panel.addClass('test');
+
+    nbTracker.activeCellChanged.connect(
+      (_, cells) => {
+        panelRender(panel, nbTracker);
+        //panel.node.textContent = cells?.model.metadata.get('comment')?.toString() as string;
+        const comment: any = cells?.model.metadata.get('comment');
+        console.log(comment.length);
+      }
+    );
+
     app.commands.addCommand(CommandIDs.addComment, {
       label: 'Add Comment',
       execute: async () => {
@@ -28,15 +44,31 @@ const plugin: JupyterFrontEndPlugin<void> = {
         if (cell == null) {
           return;
         }
-
-        void InputDialog.getText({
-          title: 'Enter Comment'
-        }).then(value => {
-          if (value.value != null) {
-            cell.model.metadata.set('comment', value.value);
-            console.log('set metadata of cell', cell.model.id);
-          }
-        });
+        const comment: any = cell.model.metadata.get('comment');
+        if(comment == null)
+        {
+          void InputDialog.getText({
+            title: 'Enter Comment'
+          }).then(value => {
+            if (value.value != null) {
+              cell.model.metadata.set('comment', [value.value]);
+              console.log('set metadata of cell', cell.model.id);
+              panelRender(panel, nbTracker);
+            }
+          });
+        }
+        else
+        {
+          void InputDialog.getText({
+            title: 'Enter Reply'
+          }).then(value => {
+            if (value.value != null) {
+              cell.model.metadata.set('comment', [...comment, value.value]);
+              console.log('set metadata of cell', cell.model.id);
+              panelRender(panel, nbTracker);
+            }
+          });
+        }
       }
     });
 
@@ -50,7 +82,9 @@ const plugin: JupyterFrontEndPlugin<void> = {
 
         const comment = cell.model.metadata.get('comment');
         if (comment != null) {
-          alert(comment);
+          console.log(typeof comment);
+          console.log(comment instanceof Array);
+          alert(comment.toString());
         }
       }
     });
@@ -66,7 +100,30 @@ const plugin: JupyterFrontEndPlugin<void> = {
       selector: '.jp-Notebook .jp-Cell',
       rank: 14
     });
+    
   }
 };
 
 export default plugin;
+
+function panelRender(panels: Widgets.Panel, tracker: INotebookTracker)
+{
+  
+  while(panels.widgets.length > 0)
+  {
+    panels.widgets[panels.widgets.length-1].dispose();
+  }
+  
+  const cell = tracker.currentWidget?.content.activeCell;
+  const comment: any = cell?.model.metadata.get('comment') as string;
+  let i = 0;
+  while(i < comment.length)
+  {
+    let newWidget = new Widgets.Panel();
+    newWidget.addClass('subclass');
+    newWidget.node.textContent = comment[i];
+    panels.addWidget(newWidget);
+    i++;
+  }
+  return;
+}
