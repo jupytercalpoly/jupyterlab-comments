b818c0d93fb11b3dc2c71be6d549e3f28b3bfff2
Cameron Toy <cameron-toy@users.noreply.github.com>
2021-06-28 17:45:43 -0700

    started work on react comment widget

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,11 @@ import { InputDialog } from '@jupyterlab/apputils';
 import * as Widgets from '@lumino/widgets';
 import * as Icons from '@jupyterlab/ui-components';
 import { INotebookTracker } from '@jupyterlab/notebook';
+import { addComment, getComments } from './comments';
+import { UUID } from '@lumino/coreutils';
+import { IComment } from './commentformat';
+import { CommentWidget } from './widget';
+import { Cell } from '@jupyterlab/cells';
 
 namespace CommandIDs {
   export const addComment = 'jl-chat:add-comment';
@@ -21,21 +26,15 @@ const plugin: JupyterFrontEndPlugin<void> = {
   autoStart: true,
   requires: [INotebookTracker],
   activate: (app: JupyterFrontEnd, nbTracker: INotebookTracker) => {
-
     let panel = new Widgets.Panel();
-    panel.id = "Comments";
+    panel.id = 'Comments';
     panel.title.icon = Icons.listIcon;
-    app.shell.add(panel, 'right', {rank:500});
+    app.shell.add(panel, 'right', { rank: 500 });
     panel.addClass('test');
 
-    nbTracker.activeCellChanged.connect(
-      (_, cells) => {
-        panelRender(panel, nbTracker);
-        //panel.node.textContent = cells?.model.metadata.get('comment')?.toString() as string;
-        const comment: any = cells?.model.metadata.get('comment');
-        console.log(comment.length);
-      }
-    );
+    nbTracker.activeCellChanged.connect((_, cells) => {
+      panelRender(panel, nbTracker);
+    });
 
     app.commands.addCommand(CommandIDs.addComment, {
       label: 'Add Comment',
@@ -44,31 +43,25 @@ const plugin: JupyterFrontEndPlugin<void> = {
         if (cell == null) {
           return;
         }
-        const comment: any = cell.model.metadata.get('comment');
-        if(comment == null)
-        {
-          void InputDialog.getText({
-            title: 'Enter Comment'
-          }).then(value => {
-            if (value.value != null) {
-              cell.model.metadata.set('comment', [value.value]);
-              console.log('set metadata of cell', cell.model.id);
-              panelRender(panel, nbTracker);
-            }
-          });
-        }
-        else
-        {
-          void InputDialog.getText({
-            title: 'Enter Reply'
-          }).then(value => {
-            if (value.value != null) {
-              cell.model.metadata.set('comment', [...comment, value.value]);
-              console.log('set metadata of cell', cell.model.id);
-              panelRender(panel, nbTracker);
-            }
-          });
-        }
+
+        void InputDialog.getText({
+          title: 'Enter Comment'
+        }).then(value => {
+          if (value.value != null) {
+            const comment: IComment = {
+              id: UUID.uuid4(),
+              type: 'cell',
+              author: 'Bob',
+              replies: [],
+              text: value.value
+            };
+
+            addComment(cell.model.metadata, comment);
+
+            console.log('set metadata of cell', cell.model.id);
+            panelRender(panel, nbTracker);
+          }
+        });
       }
     });
 
@@ -80,12 +73,7 @@ const plugin: JupyterFrontEndPlugin<void> = {
           return;
         }
 
-        const comment = cell.model.metadata.get('comment');
-        if (comment != null) {
-          console.log(typeof comment);
-          console.log(comment instanceof Array);
-          alert(comment.toString());
-        }
+        console.log(getComments(cell.model.metadata));
       }
     });
 
@@ -100,30 +88,33 @@ const plugin: JupyterFrontEndPlugin<void> = {
       selector: '.jp-Notebook .jp-Cell',
       rank: 14
     });
-    
   }
 };
 
 export default plugin;
 
-function panelRender(panels: Widgets.Panel, tracker: INotebookTracker)
-{
-  
-  while(panels.widgets.length > 0)
-  {
-    panels.widgets[panels.widgets.length-1].dispose();
-  }
-  
+function panelRender(panels: Widgets.Panel, tracker: INotebookTracker) {
+  panels.node.textContent = '';
+
   const cell = tracker.currentWidget?.content.activeCell;
-  const comment: any = cell?.model.metadata.get('comment') as string;
-  let i = 0;
-  while(i < comment.length)
-  {
-    let newWidget = new Widgets.Panel();
-    newWidget.addClass('subclass');
-    newWidget.node.textContent = comment[i];
-    panels.addWidget(newWidget);
-    i++;
+  if (cell == null) {
+    return;
   }
-  return;
+
+  const comments = getComments(cell.model.metadata);
+  if (comments == null) {
+    return;
+  }
+
+  console.log('comments', comments);
+
+  for (let comment of comments) {
+    const widget = new CommentWidget<Cell>({
+      identity: comment.author,
+      id: comment.id,
+      target: cell,
+      metadata: cell.model.metadata
+    });
+    panels.addWidget(widget);
+  }
 }
